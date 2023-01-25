af58a08f0bf61127ddea0082cb4c63bf8cb5886a
jdustinwilliams <jdustin@andrew.cmu.edu>
2023-01-25 15:29:09 -0500

    Implement bullet firing

--- Player.gd
+++ Player.gd
@@ -3,6 +3,10 @@ extends Sprite
 var speed = 150
 var velocity = Vector2()
 
+var bullet = preload("res://Bullet.tscn")
+
+var can_shoot = true
+
 func _process(delta):
 	velocity.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	velocity.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
@@ -10,3 +14,12 @@ func _process(delta):
 	velocity = velocity.normalized()
 	
 	global_position += speed * velocity * delta
+	
+	if Input.is_action_pressed("click") && Global.node_creation_parent != null && can_shoot:
+		Global.instance_node(bullet, global_position, Global.node_creation_parent)
+		$ReloadSpeed.start()
+		can_shoot = false
+
+
+func _on_ReloadSpeed_timeout():
+	can_shoot = true
